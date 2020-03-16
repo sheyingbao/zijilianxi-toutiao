1f34de9b3f27065e3b6ed5e57fbe2a0deea1cdd5
sheyingbao <sheyingbao.com>
2020-03-16 17:49:51 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class=''>home</div>
+<!-- 最外成容器-->
+<el-container>
+  <!-- 侧便拦 -->
+  <el-aside>
+    左侧内容
+  </el-aside>
+  <el-container>
+    <!-- 头部内容 -->
+    <el-header>头部内容</el-header>
+    <!-- 中间区域-->
+    <el-main>中间内容</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
